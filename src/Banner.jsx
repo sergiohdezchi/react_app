1ea8cbf4d8d9f8be0fa333a626dacd611a7ef477
sergio <sergiohdez.chi@gmail.com>
2020-11-02 09:37:39 -0600

    [RC-0005] - Rutas creadas

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
 const Banner = () => (
-    <div className ="main-banner img-container l-section" id="main-banner">
-        <div className ="ed-grid lg-grid-6">
-            <div className ="lg-cols-4 lg-x-2">
-                <img className ="main-banner__img" src="https://conceptodefinicion.de/wp-content/uploads/2014/03/ciudad.jpg" alt="test" />
-                <div className ="main-banner__data s-center">
-                    <p className ="t2 s-mb-0">Título del banner</p>
+    <div className ='main-banner img-container l-section' id='main-banner'>
+        <div className ='ed-grid lg-grid-6'>
+            <div className ='lg-cols-4 lg-x-2'>
+                <img className ='main-banner__img' src='https://conceptodefinicion.de/wp-content/uploads/2014/03/ciudad.jpg' alt='test' />
+                <div className ='main-banner__data s-center'>
+                    <p className ='t2 s-mb-0'>Título del banner</p>
                     <p> Subtítulo del banner</p>
-                    <a href="https://images.pexels.com/photos/265614/pexels-photo-265614.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb" target='blank' className ="button">Botón del banner</a>
+                    <a href='https://images.pexels.com/photos/265614/pexels-photo-265614.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb' target='blank' className ='button'>Botón del banner</a>
                 </div>
             </div>
         </div>
--- src/Curso.jsx
+++ src/Curso.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 
 //function
 
@@ -11,7 +12,9 @@ const persona = { 'nombre': 'Alberto', 'apellido': 'Quiron', 'edad': 29 }
 const Curso = props => (
     <article className ="card">
         <div className ="img-container s-ratio-16-9 s-radius-tr s-radius-tl">
-        <img src={ props.image } alt={ props.title } />
+            <Link to={`/cursos/${props.id}`}>
+               <img src={ props.image } alt={ props.title } />
+            </Link>
         </div>
         <div className ="card__data s-border s-radius-br s-radius-bl s-pxy-2">
         <h3 className ="t5 s-mb-2 s-center">
@@ -21,7 +24,7 @@ const Curso = props => (
             <div className ="card__teacher s-cross-center">
             <div className ="card__avatar s-mr-1">
                 <div className ="circle img-container">
-                <img src="https://edteam-media.s3.amazonaws.com/users/thumbnail/66f015b2-0dfb-4ba9-bd0d-f7a7e1855275.jpeg" alt="test" />
+                    <img src="https://edteam-media.s3.amazonaws.com/users/thumbnail/66f015b2-0dfb-4ba9-bd0d-f7a7e1855275.jpeg" alt="test" />
                 </div>
             </div>
             <span className ="small">{ props.profesor } </span>
